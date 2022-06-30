f0b375e3f3cba57cc019012a5c9d12987a479f00
Adrincovich <adrian.drincovich@gmail.com>
2022-06-30 11:48:45 -0300

    Avance en clase

---
+++ app.js
@@ -0,0 +1,20 @@
+console.log('app')
+
+var pintarTablero = [
+    [1, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0],
+    [0, 0, 3, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+
+
+var colores = {
+    VERDE: 1,
+    AMARILLO: 2,
+    GRIS: 3,
+    BLANCO: 0
+}
+
+console.log(colores, pintarTablero)
--- scripts.js
+++ scripts.js
@@ -1,31 +1,179 @@
-window.onload= function() {
+// pintarTablero();
+// const f0c0 = document.getElementById("f0c0");
+// const f0c1 = document.getElementById("f0c1");
+// const f0c2 = document.getElementById("f0c2");
+// const f0c3 = document.getElementById("f0c3");
+// const f0c4 = document.getElementById("f0c4");
+// const f1c0 = document.getElementById("f1c0");
+// const f1c1 = document.getElementById("f1c1");
+// const f1c2 = document.getElementById("f1c2");
+// const f1c3 = document.getElementById("f1c3");
+// const f1c4 = document.getElementById("f1c4");
+// const f2c0 = document.getElementById("f2c0");
+// const f2c1 = document.getElementById("f2c1");
+// const f2c2 = document.getElementById("f2c2");
+// const f2c3 = document.getElementById("f2c3");
+// const f2c4 = document.getElementById("f2c4");
+// const f3c0 = document.getElementById("f3c0");
+// const f3c1 = document.getElementById("f3c1");
+// const f3c2 = document.getElementById("f3c2");
+// const f3c3 = document.getElementById("f3c3");
+// const f3c4 = document.getElementById("f3c4");
+// const f4c0 = document.getElementById("f4c0");
+// const f4c1 = document.getElementById("f4c1");
+// const f4c2 = document.getElementById("f4c2");
+// const f4c3 = document.getElementById("f4c3");
+// const f4c4 = document.getElementById("f4c4");
+// const f5c0 = document.getElementById("f5c0");
+// const f5c1 = document.getElementById("f5c1");
+// const f5c2 = document.getElementById("f5c2");
+// const f5c3 = document.getElementById("f5c3");
+// const f5c4 = document.getElementById("f5c4");
+
+// const fila0 = document.getElementById("fila0");
+// const fila1 = document.getElementById("fila1");
+// const fila2 = document.getElementById("fila2");
+// const fila3 = document.getElementById("fila3");
+// const fila4 = document.getElementById("fila4");
+// const fila5 = document.getElementById("fila5");
+
+// const valida0 = [`${f0c0}, ${f0c1}, ${f0c2}, ${f0c3}, ${f0c4}`];
+// const valida1 = [`${f1c0}, ${f1c1}, ${f1c2}, ${f1c3}, ${f1c4}`];
+// const valida2 = [`${f2c0}, ${f2c1}, ${f2c2}, ${f2c3}, ${f2c4}`];
+// const valida3 = [`${f3c0}, ${f3c1}, ${f3c2}, ${f3c3}, ${f3c4}`];
+// const valida4 = [`${f4c0}, ${f4c1}, ${f4c2}, ${f4c3}, ${f4c4}`];
+// const valida5 = [`${f5c0}, ${f5c1}, ${f5c2}, ${f5c3}, ${f5c4}`];
 
 
 
-let matrizWoldle = [
-    [0, 1, 2, 3, 4],
-    [1, 2, 3, 4, 5],
-    [2, 3, 4, 5, 6],
-    [3, 4, 5, 6, 7],
-    [4, 5, 6, 7, 8],
-    [5, 6, 7, 8, 9],
+
+// fila0.addEventListener("keyup", function () {
+//     if (f0c0.value.length == 1 &&
+//         f0c1.value.length == 1 &&
+//         f0c2.value.length == 1 &&
+//         f0c3.value.length == 1 &&
+//         f0c4.value.length == 1){
+//         console.log("hola")
+//     }
+// })
+
+// fila0.addEventListener("keyup", function () {
+//     for (var i = 0; i < valida0.length; i++) {
+//         if (valida0[i].value == 1){
+//             console.log("hola")
+//         }
+//     }
+// })
+
+
+
+
+
+
+
+/*
+vamos a crear 2 funciones cuando se abre el navegador
+inicio();
+pintarTablero();
+
+*/
+
+//matriz colores del tablero
+
+var colorTablero = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
 ]
 
-console.log(matrizWoldle);
+// necesitamos variable colores:
 
+var colores = {
+    VERDE: 1,
+    AMARILLO: 2,
+    GRIS: 3,
+    BLANCO: 0
+}
 
-//se usa un for de otro for para meterse en todos los valores en una matriz
-//un for para ese arreglo, nos posiciona en 0
-/* y otro for para recorrer dentro de ese arreglo.
-de la matriz i imprime el resultado-- cuando j llega al maximo sale
-y vuelve a entrar hasta que el valor de i sea 1*/
-
-
-for (i=0; i<matrizWoldle.length; i++) {
-    for (j=0; j<matrizWoldle[i].length; j++){
-        document.getElementById(`f${i}c${j}`).value = matrizWoldle[i][j];
-        console.log(matrizWoldle[i][j]);
+function pintarTablero(){
+    for (let iFila = 0; iFila < 6; iFila++) {
+        for (let iCol=0; iCol<5; iCol++){
+            let input = document.getElementById(`f${iFila}c${iCol}`)
+            switch(colorTablero[iFila][iCol]){
+                case colores.VERDE:
+                    input.classList.add("verde");
+                    break;
+                case colores.AMARILLO:
+                    input.classList.add("amarillo");
+                    break;
+                case colores.GRIS:
+                    input.classList.add("gris");
+                    break;
+                case colores.BLANCO:
+                    input.classList.add("blanco");
+                    break;
+            }
+        }
     }
 }
 
+
+var respuestas = [
+    [],//
+    [],
+    [],
+    [],
+    [],
+    [],
+]
+
+function inicio () {
+    for (let indice = 0; indice < 6; indice++){
+        let fieldset = document.getElementById(`fila${indice}`);
+        fieldset.onkeydown = function (event){
+            if(event.key === `Enter`){
+                //console.log('indice',indice)
+                guardarRespuesta(indice)
+            }
+        }
+    }
 }
+
+function guardarRespuesta(indice){
+    //console.log(indice)
+    for (let iCol = 0; iCol < 5; iCol++){
+        let input = document.getElementById(`f${indice}c${iCol}`).value;
+        respuestas[indice].push(input);
+    }
+    revisarResultado(respuestas[indice], indice);
+}
+
+
+
+var palabraGanadora = "birra"
+var arrayPalabraGanadora = palabraGanadora.split("")
+
+
+
+function revisarResultado(respuesta, indice){
+    respuesta.forEach(function(elemento, index){
+        if(elemento === arrayPalabraGanadora[index] ){
+            colorTablero[indice][index] = colores.VERDE;
+        }
+    })
+    pintarTablero();
+}
+
+
+
+window.onload = function(){
+    //console.log(palabraGanadora.split(""))
+    inicio()
+    pintarTablero()
+}
+
+
+
